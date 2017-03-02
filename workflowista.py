a182361bd2f61d6e9558bb30dbf882447f699a33
David Seibert <pdavidseibert@gmail.com>
2017-03-02 18:34:34 -0500

    refactor

--- workflowista.py
+++ workflowista.py
@@ -1,100 +1,105 @@
 from flask import Flask, request
 import asyncio
 import unittest
+from unittest import TestCase, skipUnless, skipIf, skip
 from urllib.parse import quote
 import webbrowser
 import logging
 import json
-from json import loads as decode
-from sys import exc_info
+
+werkzeug_log = logging.getLogger('werkzeug')
+werkzeug_log.setLevel(logging.DEBUG)
+
+class LoggerMixin (object):
+    def __init__(self):
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.log.setLevel(logging.DEBUG)
+        
+    def __repr__(self):
+        return "{}()".format(self.__class__.__name__)
 
 class WorkflowOutputTest(unittest.TestCase):
     def setUp(self):
-        workflow = Workflow('Client')
-        self.output = workflow.run()
-        self.test_output_is_decodable()
-    
-    def test_output_is_decodable(self):
+        self.workflow = Workflow()
+        self.output = self.workflow.run('Client')
+
+    def test_output_is_encodable(self):
         try:
-            jsondata = decode(self.output)
+            encoded_json = json.dumps(self.output)
         except Exception as e:
             self.failureException = type(e)
             self.fail(e)
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+    def test_output_contents(self):
+        expected = [{'greeting': 'Hello'}]
+        self.assertListEqual(self.output, expected)
 
-def logged(func):
-    def newfunc(*args, **kwargs):
-        log.debug('entering {}: args={}, kwargs={}'.format(func.__name__, args, kwargs))
-        res = func(*args, **kwargs)
-        log.debug('leaving {}'.format(func.__name__))
-        return res
-    return newfunc
-
-def sleep(func):
-    async def newfunc(*args, **kwargs):
-        log.debug('sleeping in {}'.format(func.__name__))
-        await asyncio.sleep(0.1)
-        log.debug('done sleeping in {}'.format(func.__name__))
-        res = logged(func)(*args, **kwargs)
-        return res
-    return newfunc
-
-
-class Workflow (object):
-    SCHEME = "workflow://x-callback-url/run-workflow?name={}&x-success=pythonista://"
-    def __init__(self, name):
-        self.name = name
-        self.data = []
-        self.receiver = Flask(__name__)
-        
-        @self.receiver.route('/', methods = ['POST'])
-        @logged
+class Receiver(LoggerMixin):    
+    output = []
+    def __init__(self):
+        super().__init__()
+        self.flask = Flask(__name__)
+        @self.flask.route('/', methods = ['POST'])
         def index():
-            self.data.append(request.get_json())
-            shutdown = logged(request.environ.get('werkzeug.server.shutdown'))
+            
+            self.log.info(' -- received POST request')
+            self.output.append(request.get_json())
+            shutdown = request.environ.get('werkzeug.server.shutdown')
             shutdown()
             return "<h1>Thanks!</h1><p>Shutting down now...</p>"
-        
-    @property
-    def url(self):
-        return self.SCHEME.format(quote(self.name))
-        
-    def __repr__(self):
-        return "Workflow({})".format(repr(self.name))
+    
+    async def listen(self):
+        self.log.info('sleeping')
+        await asyncio.sleep(0.1)
+        self.log.info('started listening')
+        self.flask.run()
+        self.log.info('stopped listening')
 
-    @logged
-    def run(self):
+class Transmitter(LoggerMixin):
+    def make_url(self, name):
+        SCHEME = "".join([
+        "workflow://",
+        "x-callback-url/",
+        "run-workflow",
+        "?name={}",
+        "&x-success=pythonista://",
+        ])
+        return SCHEME.format(quote(name))
+
+    async def send_request(self, name):
+        self.log.info('sleeping')
+        await asyncio.sleep(0.1)
+        self.log.info('sending')
+        webbrowser.open(self.make_url(name))
+        self.log.info('sent')
+
+class Workflow (LoggerMixin):
+    def __init__(self):
+        super().__init__()
+        self.rx = Receiver()
+        self.tx = Transmitter()
+
+    def run(self, name):
+        self.log.info('running {}'.format(repr(name)))
+        self.log.info('loop starting')
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.main())
-        #return self.data
-        return self.encode(self.data)
+        loop.run_until_complete(self.connection(name))
+        self.log.info('loop finished')
+        self.log.info('received output: {}'.format(type(self.rx.output)))
+        return self.rx.output
+
+    async def connection(self, name):
+        future_response = self.schedule(self.rx.listen)
+        self.log.info('waiting for request')
+        await self.tx.send_request(name)
+        self.log.info('waiting for response')
+        await future_response
     
-    @staticmethod
-    def encode(data):
-        encoder = json.JSONEncoder()
-        encoded = encoder.encode(data)
-        return encoded
-    
-    @logged
-    async def main(self):
-        response = asyncio.ensure_future(self.get_response())
-        await self.send_request()
-        await response
+    def schedule(self, func):
+        task = asyncio.ensure_future(func())
+        return task
 
-    @sleep
-    def get_response(self):        
-        self.receiver.run()
+if __name__ == '__main__':
+    workflow = Workflow()
+    output = workflow.run('Client')
 
-    @sleep
-    def send_request(self):
-        webbrowser.open(self.url)
-
-
-
-
-if __name__ == "__main__":
-    workflow = Workflow("Client")
-    data = workflow.run()
-    log.info(data)    
